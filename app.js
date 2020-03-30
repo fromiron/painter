a13f8087618624b2bb5e60333251d3168247d1f4
Fromiron <artless1112@gmail.com>
2020-03-30 13:03:48 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -9,12 +11,22 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.0;
 
 let painting = false;
+let filling = false;
 
 const stopPainting = () => painting = false;
 const startPainting = () => painting = true;;
-const handleColorClick = (event) =>
+const handleColorChange = (event) =>
     ctx.strokeStyle = event.target.style.backgroundColor;
 
+const handleModeChange = () => {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
 
 const onMouseMove = event => {
     const x = event.offsetX;
@@ -28,9 +40,9 @@ const onMouseMove = event => {
     }
 };
 
-
-
-
+const handleRangeChange = range => {
+    ctx.lineWidth = event.target.value;
+};
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -40,5 +52,13 @@ if (canvas) {
 }
 
 Array.from(colors).forEach(color =>
-    color.addEventListener("click", handleColorClick)
+    color.addEventListener("click", handleColorChange)
 );
+
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+};
+
+if (mode) {
+    mode.addEventListener("click", handleModeChange);
+};
